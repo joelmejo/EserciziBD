77ef85a51d63ea3a7a09d86ee8ddcffcfb35bdd5
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-10-20 21:16:26 +0200

    1-4 done

--- Accademia/Accademia7/query.sql
+++ Accademia/Accademia7/query.sql
@@ -1,6 +1,6 @@
 -- 1. Qual è media e deviazione standard degli stipendi per ogni categoria di strutturati?
 
-SELECT posizione, avg(p.stipendio) AS media, stddev_samp(p.stipendio) AS deviazione_standard
+SELECT posizione, avg(p.stipendio) AS media, stddev(p.stipendio) AS deviazione_standard
 FROM Persona
 GROUP BY posizione;
 
@@ -17,21 +17,65 @@ GROUP BY posizione;
 WITH media AS (
     SELECT posizione, avg(stipendio) as media_stipendio
     FROM Persona
+    WHERE posizione = 'Ricercatore'
     GROUP BY posizione
 )
 
-SELECT p.*, m.media_stipendio
+SELECT p.*
 FROM Persona p, media as m
 WHERE p.stipendio > m.media_stipendio
     AND p.posizione = m.posizione;
 
+ id |  nome   |  cognome   |  posizione  | stipendio 
+----+---------+------------+-------------+-----------
+  0 | Anna    | Bianchi    | Ricercatore |   45500.3
+  2 | Barbara | Burso      | Ricercatore |   40442.5
+ 12 | Dario   | Basile     | Ricercatore |     42566
+ 20 | Carla   | Martinelli | Ricercatore |   42030.2
+(4 rows)
+
 -- 3. Per ogni categoria di strutturati quante sono le persone con uno stipendio che
 -- differisce di al massimo una deviazione standard dalla media della loro categoria?
 
+WITH stde AS (
+    SELECT posizione, avg(stipendio) AS media, stddev(stipendio) AS deviazione_standard
+    FROM Persona
+    GROUP BY posizione
+)
 
+SELECT p.posizione, count(*) AS numero
+FROM Persona p, stde s
+WHERE p.posizione = s.posizione
+    AND (p.stipendio <= s.media + s.deviazione_standard AND p.stipendio >= s.media - s.deviazione_standard)
+GROUP BY p.posizione;
+
+      posizione       | numero 
+----------------------+--------
+ Professore Ordinario |      4
+ Professore Associato |      6
+ Ricercatore          |      4
+(3 rows)
 
 -- 4. Chi sono gli strutturati che hanno lavorato almeno 20 ore complessive in attività
 -- progettuali? Restituire tutti i loro dati e il numero di ore lavorate.
+
+WITH ore AS (
+    SELECT p.id, sum(ap.oreDurata) AS somma
+    FROM Persona p, AttivitaProgetto ap
+    WHERE p.id = ap.persona
+    GROUP BY p.id
+)
+
+SELECT p.*, o.somma as ore_lavorate
+FROM Persona p, ore o
+WHERE p.id = o.id
+    AND o.somma >= 20;
+
+ id | nome | cognome |  posizione  | stipendio | ore_lavorate 
+----+------+---------+-------------+-----------+--------------
+  0 | Anna | Bianchi | Ricercatore |   45500.3 |           38
+(1 row)
+
 -- 5. Quali sono i progetti la cui durata è superiore alla media delle durate di tutti i
 -- progetti? Restituire nome dei progetti e loro durata in giorni.
 -- 6. Quali sono i progetti terminati in data odierna che hanno avuto attività di tipo
